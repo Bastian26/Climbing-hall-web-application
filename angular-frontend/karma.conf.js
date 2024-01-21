85ed47010d12783c4a0a86a56109ecb6422f4c24
Bastian <b.struggl@gmx.de>
2024-01-21 19:33:20 +0100

    changed karma file

--- angular-frontend/karma.conf.js
+++ angular-frontend/karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
         require('karma-chrome-launcher'),
         require('karma-jasmine-html-reporter'),
         require('karma-coverage-istanbul-reporter'),
-        require('@angular-devkit/build-angular/plugins/karma')
+        require('@angular/cli/plugins/karma')
       ],
       client: {
         jasmine: {
@@ -25,7 +25,7 @@ module.exports = function(config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['Chrome'],
+      browsers: ['ChromeHeadless'],
       singleRun: false,
       restartOnFileChange: true
     });
